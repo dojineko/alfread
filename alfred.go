c580b8ba648b0e57e9c2d93c22079eedf0beb680
dojineko <dojineko.spiral@gmail.com>
2016-04-02 20:31:32 +0900

    Workflow向けの構造体を追加

---
+++ alfred.go
@@ -0,0 +1,57 @@
+package alfred
+
+import (
+	"encoding/xml"
+	"log"
+)
+
+// XMLHeader is definition for XML format.
+const XMLHeader = `<?xml version="1.0"?>`
+
+// Item contains value list item.
+type Item struct {
+	UID          string     `xml:"uid,attr,omitempty"`
+	Arg          string     `xml:"arg,attr,omitempty"`
+	Valid        bool       `xml:"valid,attr,omitempty"`
+	Autocomplete string     `xml:"autocomplete,attr,omitempty"`
+	File         string     `xml:"file,attr,omitempty"`
+	Type         string     `xml:"type,attr,omitempty"`
+	Title        string     `xml:"title"`
+	Subtitles    []Subtitle `xml:"subtitle,omitempty"`
+	Icons        []Icon     `xml:"icon,omitempty"`
+	Texts        []Text     `xml:"text,omitempty"`
+}
+
+// Subtitle contains values for Subtitle.
+type Subtitle struct {
+	Value string `xml:",chardata"`
+	Mod   string `xml:"attr,omitempty"`
+}
+
+// Text contains values for Text.
+type Text struct {
+	Value string `xml:",chardata"`
+	Type  string `xml:"type,attr,omitempty"`
+}
+
+// Icon contains values for Icon.
+type Icon struct {
+	Value string `xml:",chardata"`
+	Type  string `xml:"type,attr,omitempty"`
+}
+
+// Marshal output XML for Alfred as string.
+func Marshal(items []Item) string {
+	str, err := xml.MarshalIndent(
+		struct {
+			XMLName struct{} `xml:"items"`
+			Items   []Item   `xml:"item"`
+		}{Items: items}, "", "  ")
+
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+
+	return XMLHeader + "\n" + string(str) + "\n"
+}
